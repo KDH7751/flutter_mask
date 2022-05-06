055df2fc67a42b24b38bd8d61a03d469a7fb91e0
KDH7751 <pnix7649@gmail.com>
2022-05-06 12:52:39 +0900

    1. 작동 안되던 재고 카운트 기능 정상화 2. 주석 수정 3. 일정 이하 카운트 안하는 부분을 메서드로 바꿔서 중복코드 감소

--- lib/View/getmyhomepage.dart
+++ lib/View/getmyhomepage.dart
@@ -14,13 +14,11 @@ class GetMyHomePage extends StatelessWidget { //stf -> stl로 변경
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('마스크 재고 있는 곳 : ${GetMyHomepageController.stores
-            .where((e) { //일정 이하는 표시 안하기 위한것
-          return e.remainStat =='plenty' ||
-              e.remainStat =='some' ||
-              e.remainStat =='few';
-        })
-            .length}곳'),
+        title: Obx(() { //obx로 묶어줘야 카운트가 제대로 됨
+          return Text('마스크 재고 있는 곳 : ${storesCut()
+              .length}곳');
+
+        }),
         actions: <Widget>[
           IconButton(icon: Icon(Icons.refresh),onPressed: GetMyHomepageController.fetch,) //새로고침 버튼 눌렀을때 fetch를 하라고 함,GetMyHomepageController.fetch로 변경
         ],
@@ -29,29 +27,27 @@ class GetMyHomePage extends StatelessWidget { //stf -> stl로 변경
       body: Obx(() { //여기 참고할 것, obx랑 builder로 감쌌음.()가 빌더에 해당하는 부분
         return GetMyHomepageController.isLoading.value //GetMyHomepageController.isLoading.value로 변경
             ? loadingWidget() : ListView( //리스트뷰. 리스트 형태로 보여줌. & 로딩 중일때랑 아닐때 분기를 나눠서 표시해야 되기 때문에 ?로 확인
-          children: GetMyHomepageController.stores //GetMyHomepageController.stores로 변경
-              .where((e) { //일정 이하는 표시 안하기 위한것
-            return e.remainStat =='plenty' ||
-                e.remainStat =='some' ||
-                e.remainStat =='few';
-          })
+          children: storesCut()
               .map((e){
             return ListTile( //여기가 약국이름(title)&위치(subtitle)&오른쪽(trailing) 리스트로 보여주는 부분.
               title: Text(e.name),
               subtitle: Text(e.addr),
               trailing: _buildRemainStatWidget(e),
             );
-          })
-              .
-          toList
-            (
-          )
-          ,
+          }).toList(),
         );
-
       }),
     );
   }
+
+  Iterable<Store> storesCut() { //일정 이하는 표시 안하기 위한 것, 2번 사용되서 밖으로 따로 뺐음
+    return GetMyHomepageController.stores.where((e) {
+          return e.remainStat =='plenty' ||
+              e.remainStat =='some' ||
+              e.remainStat =='few';
+        });
+  }
+
   Widget _buildRemainStatWidget(Store store){ //오른쪽 부분에 색깔 입히고, 상태별로 글자 종류바꿔서 띄워주기 위한 위젯
     var remainStat = '판매중지';
     var description = '판매중지';
